90aeaab4c6a0c4fed2b28308c4f2572f850bc82c
Pauline Telan <pauline.telan@gmail.com>
2020-09-09 22:13:47 -0600

    adding keyboard press handling

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
+function playSound(keyLetter) {
+  console.log(keyLetter);
+  switch (keyLetter) {
+    case "w":
+      audioObj = new Audio("sounds/crash.mp3");
+      audioObj.play();
+      break;
+    case "a":
+      audioObj = new Audio("sounds/kick-bass.mp3");
+      audioObj.play();
+      break;
+    case "s":
+      audioObj = new Audio("sounds/snare.mp3");
+      audioObj.play();
+      break;
+    case "d":
+      audioObj = new Audio("sounds/tom-1.mp3");
+      audioObj.play();
+      break;
+    case "j":
+      audioObj = new Audio("sounds/tom-2.mp3");
+      audioObj.play();
+      break;
+    case "k":
+      audioObj = new Audio("sounds/tom-3.mp3");
+      audioObj.play();
+      break;
+    case "l":
+      audioObj = new Audio("sounds/tom-4.mp3");
+      audioObj.play();
+      break;
+    default:
+      console.log("Button not found!");
+  }
+}
 
+// Detect mouse click
 var buttons_array = document.querySelectorAll("button");
 
-for (var i=0; i < buttons_array.length; i++){
-  document.querySelectorAll("button")[i].addEventListener("click", function (){
-    switch (buttonInnerHTML = this.innerHTML) {
-      case "w":
-        soundFile = "crash"
-        break;
-      case "a":
-        soundFile = "kick-bass";
-        break;
-      case "s":
-        soundFile = "snare";
-        break;
-      case "d":
-        soundFile = "tom-1";
-        break;
-        case "j":
-          soundFile = "tom-2";
-          break;
-        case "k":
-          soundFile = "tom-3";
-          break;
-        case "l":
-          soundFile = "tom-4";
-          break;
-      default:
-        console.log("Button not found!");
-
-    }
-
-
-    audioObj = new Audio("sounds/"+ soundFile +".mp3");
-    audioObj.play();
+for (var i = 0; i < buttons_array.length; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
+    playSound(this.innerHTML);
   });
 }
+
+// Detect keyboard press
+document.addEventListener("keydown", event => {
+  playSound(event.key);
+});
